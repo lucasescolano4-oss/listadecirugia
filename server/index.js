86e24ea9cbde8257e8b8c6d9c6b23edb8db1ae48
Lucas Escolano <lucasescolano4-oss@users.noreply.github.com>
2026-02-15 22:03:31 -0300

    preparar para despliegue en la nube

--- server/index.js
+++ server/index.js
@@ -201,7 +201,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
 });
